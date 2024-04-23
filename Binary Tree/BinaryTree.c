0f5d0a1cddbbc1b6e93df2e4b06de5fe14e30024
Gabriel <gbmagalhaes1996@gmail.com>
2024-04-23 13:34:22 -0300

    update

--- Binary Tree/BinaryTree.c
+++ Binary Tree/BinaryTree.c
@@ -1,4 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct no {
+	int conteudo;
+	struct no *esquerda, *direito;
+}No;
+
+typedef struct {
+	No* raiz; //raiz da árvore binária
+}BinaryTree;
+
+void inserirEsquerda(No* no, int valor) {
+
+}
+
+void inserirDireita(No* no, int valor) {
+
+}
+
+void inserir(BinaryTree* arv, int valor) {
+	if (arv->raiz == NULL) {
+		No* novo = (No*)malloc(sizeof(No));
+		novo->conteudo = valor;
+		novo->esquerda = NULL;
+		novo->direito = NULL;
+		arv->raiz = novo;
+	}
+	else {
+		if (valor < arv->raiz->conteudo)
+			inserirEsquerda(arv->raiz, valor);
+		else
+			inserirDireita(arv->raiz, valor);
+	}
+}
 
 int main() {
 	printf("Hello, World!");
